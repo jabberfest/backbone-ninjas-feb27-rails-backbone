b8c0e821b74e6d947ebcbe530096e1442d92b2ce
sandbox <sandbox@ubuntu.(none)>
2013-02-27 08:31:10 -0800

    Added Controller Logic

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,18 +1,85 @@
 class TodoController < ApplicationController
 
-	def index
-	end
+ # GET /todo
+  # GET /todo.json
+  def index
+    @todos = Todo.all
 
-	def show
-	end
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @todos }
+    end
+  end
 
-	def update
-	end
+  # GET /todo/1
+  # GET /todo/1.json
+  def show
+    @todo = Todo.find(params[:id])
 
-	def create
-	end
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @todo }
+    end
+  end
 
-	def destroy
-	end
+  # GET /todos/new
+  # GET /todos/new.json
+  def new
+    @todo = Todo.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @todo }
+    end
+  end
+
+  # GET /todo/1/edit
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  # POST /todo
+  # POST /todo.json
+  def create
+    @todo = Todo.new(params[:todo])
+
+    respond_to do |format|
+      if @todo.save
+        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.json { render json: @todo, status: :created, location: @todo }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @todo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /todo/1
+  # PUT /todo/1.json
+  def update
+    @todo = Todo.find(params[:id])
+
+    respond_to do |format|
+      if @todo.update_attributes(params[:todo])
+        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @todo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /todo/1
+  # DELETE /todo/1.json
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+
+    respond_to do |format|
+      format.html { redirect_to todos_url }
+      format.json { head :no_content }
+    end
+  end
 
 end
